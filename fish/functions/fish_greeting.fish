b3522acc5a0b1a2b9948005f4297d1fa36cf5df0
Jariel Que <kawaii@duck.com>
2021-11-01 08:33:18 +0800

    update fish greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,7 +1,7 @@
 function fish_greeting
     bash -c "echo $none"
-    bash -c "echo -e \"\e[0;32mWelcome back \e[0;35m$USER@$hostname\e[0;32m!\e[0;37m\""
+    bash -c "echo -e \"\e[0;32mWelcome back \e[0;34m$USER@$hostname\e[0;32m!\e[0;37m\""
     bash -c "echo $none"
-    bash -c "echo -e \"You are currently on \e[0;34mArch \$(uname -srm)\e[0;0m!\""
+    bash -c "echo -e \"You are currently on \e[0;33mArch \$(uname -srm)\e[0;0m!\""
     bash -c "echo -e \"\e[0;37mIt is currently \$(date).\""
 end
